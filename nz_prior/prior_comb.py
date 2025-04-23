e29412932fab62be4ddd722154127bd8f49c79c5
jaimerzp <jaimerz011235813@gmail.com>
2025-04-23 15:38:26 +0100

    bug

--- nz_prior/prior_comb.py
+++ nz_prior/prior_comb.py
@@ -19,7 +19,7 @@ class PriorComb(PriorLinear):
         dz = (zmax - zmin) / self.n
         zmeans = [(zmin + dz / 2) + i * dz for i in range(self.n)]
         combs = [norm(zmeans[i], dz / 2) for i in np.arange(self.n)]
-        combs = np.array([comb.pdf(self.z) for comb in combs])
+        combs = np.array([comb.pdf(self.z) for comb in combs]).T
         return combs
 
     def _get_weights(self):
